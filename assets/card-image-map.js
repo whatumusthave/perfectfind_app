f40f33072103dc4fb0173a01b0575aa7e2613277
Sia Yoon <siayoon@Sias-MacBook-Pro.local>
2026-05-09 17:50:40 -0700

    Final patch: fix relative card paths, hero/collection images, stage1 front-card brightness

--- assets/card-image-map.js
+++ assets/card-image-map.js
@@ -1,18 +1,18 @@
 window.CARD_IMAGE_MAP = {
-  "golden-paw": "/assets/cards/golden-paw.png",
-  "indigo-bowtie": "/assets/cards/indigo-bowtie.png",
-  "fuchsia-ribbon": "/assets/cards/fuchsia-ribbon.png",
-  "crystal-ball": "/assets/cards/crystal-ball.png",
-  "royal-cat-bed": "/assets/cards/royal-cat-bed.png",
-  "silver-bag": "/assets/cards/silver-bag.png",
-  "celestial-potion": "/assets/cards/celestial-potion.png",
-  "starry-mic": "/assets/cards/starry-mic.png",
-  "amethyst-heart": "/assets/cards/amethyst-heart.png",
-  "midnight-cushion": "/assets/cards/midnight-cushion.png",
-  "sapphire-paw": "/assets/cards/sapphire-paw.png",
-  "mystic-yarn": "/assets/cards/mystic-yarn.png",
-  "jeweled-key": "/assets/cards/jeweled-key.png",
-  "rose-crystal": "/assets/cards/rose-crystal.png"
+  "golden-paw": "./assets/cards/golden-paw.png",
+  "indigo-bowtie": "./assets/cards/indigo-bowtie.png",
+  "fuchsia-ribbon": "./assets/cards/fuchsia-ribbon.png",
+  "crystal-ball": "./assets/cards/crystal-ball.png",
+  "royal-cat-bed": "./assets/cards/royal-cat-bed.png",
+  "silver-bag": "./assets/cards/silver-bag.png",
+  "celestial-potion": "./assets/cards/celestial-potion.png",
+  "starry-mic": "./assets/cards/starry-mic.png",
+  "amethyst-heart": "./assets/cards/amethyst-heart.png",
+  "midnight-cushion": "./assets/cards/midnight-cushion.png",
+  "sapphire-paw": "./assets/cards/sapphire-paw.png",
+  "mystic-yarn": "./assets/cards/mystic-yarn.png",
+  "jeweled-key": "./assets/cards/jeweled-key.png",
+  "rose-crystal": "./assets/cards/rose-crystal.png"
 };
 window.CARD_ORDER = [
   "golden-paw","indigo-bowtie","fuchsia-ribbon","crystal-ball",
@@ -21,5 +21,5 @@ window.CARD_ORDER = [
   "jeweled-key","rose-crystal"
 ];
 window.cardImgSrc = function(key){
-  return (window.CARD_IMAGE_MAP && window.CARD_IMAGE_MAP[key]) || "/assets/cards/card-backup.png";
+  return (window.CARD_IMAGE_MAP && window.CARD_IMAGE_MAP[key]) || "./assets/cards/card-backup.png";
 };
